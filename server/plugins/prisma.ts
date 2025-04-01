78f6ee3f1614cc1dd499ec87e526196e1d620806
sidekickweb <sidekickwebdev@gmail.com>
2025-04-01 15:22:27 +0000

    fix: update Prisma client imports to reference index.js file - Fix EISDIR error by pointing to specific file instead of directory

--- server/plugins/prisma.ts
+++ server/plugins/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/.prisma/client'
+import { PrismaClient } from '@prisma/client/.prisma/client/index.js'
 import { defineNitroPlugin } from 'nitropack/runtime'
 
 // Create a singleton Prisma instance
