77a5ce834936553d5a7a78d8da1c2e35a8f26eea
sidekickweb <sidekickwebdev@gmail.com>
2025-04-01 15:17:17 +0000

    fix: update Prisma configuration for Windows compatibility - Set custom output path for Prisma client - Update import path to use custom location

--- server/plugins/prisma.ts
+++ server/plugins/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/.prisma/client'
 import { defineNitroPlugin } from 'nitropack/runtime'
 
 // Create a singleton Prisma instance
