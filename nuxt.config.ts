bce439a850c06c3cf336f3de56fb25365f1179b0
sidekickweb <sidekickwebdev@gmail.com>
2025-04-01 14:31:32 +0000

    Update build configuration for Vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
     },
     optimizeDeps: {
       exclude: ['@prisma/client']
+    },
+    ssr: {
+      noExternal: ['@prisma/client']
     }
   },
   runtimeConfig: {
---
+++ prisma/prisma.config.js
@@ -0,0 +1,5 @@
+export default {
+  client: {
+    engineType: process.env.PRISMA_CLIENT_ENGINE_TYPE || 'binary'
+  }
+} 
