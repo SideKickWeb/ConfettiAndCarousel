0525d58c391bcb83bd004a577bf819743a148b6a
Owen Phillips <ojphillips2007@gmail.com>
2025-06-02 21:29:04 +0100

    Fix Prisma import paths to include .js extension for Rollup compatibility

--- server/api/bookings/index.js
+++ server/api/bookings/index.js
@@ -1,4 +1,4 @@
-import prisma from '../../../lib/prisma'
+import prisma from '../../../lib/prisma.js'
 import { getMethod, readBody, getQuery, getCookie } from 'h3'
 import jwt from 'jsonwebtoken'
 
